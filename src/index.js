3984b23efe4e13008b8042efc3bbbb304fd77ee7
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 20:33:50 -0500

    test

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-  <BrowserRouter basename="/lite-up-acrylics">
-    <App />
+  // basename="/lite-up-acrylics"
+  <BrowserRouter>
+    {/* <App /> */}
+    <p>Hello World</p>
   </BrowserRouter>
   //</React.StrictMode>
 );
