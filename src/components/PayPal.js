70799a8dd0c908b4589fd3129ee6c5373ea77f45
BuildTools <unconfigured@null.spigotmc.org>
2023-05-17 14:29:32 -0500

    live payment

--- src/components/PayPal.js
+++ src/components/PayPal.js
@@ -10,67 +10,83 @@ export default function PayPal({
   setCartCount,
   setPurchased,
 }) {
-  async function deleteOrdersForUser() {
-    try {
-      const user = await Auth.currentAuthenticatedUser();
-      const userId = user.attributes.sub;
-      const result = await API.graphql(
-        graphqlOperation(listOrders, {
-          filter: {
-            userId: { eq: userId },
+  const paypal = useRef();
+  const paypalClientId = process.env.REACT_APP_PAYPAL_CLIENT_ID;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://www.paypal.com/sdk/js?client-id=${paypalClientId}&currency=USD`;
+    script.onload = () => {
+      window.paypal
+        .Buttons({
+          createOrder: (data, actions, err) => {
+            return actions.order.create({
+              intent: "CAPTURE",
+              purchase_units: [
+                {
+                  description: description,
+                  amount: {
+                    currency_code: "USD",
+                    value: value,
+                  },
+                },
+              ],
+            });
+          },
+          onApprove: async (data, actions) => {
+            try {
+              await actions.order.capture();
+              setCartCount(0);
+              setAlbumsInCart([]);
+              setPurchased(true);
+              deleteOrdersForUser();
+            } catch (err) {
+              console.log(err);
+            }
+          },
+          onError: (err) => {
+            console.log(err);
           },
         })
-      );
-      const orderIds = result.data.listOrders.items.map((item) => item.id);
-      await Promise.all(
-        orderIds.map((orderId) =>
-          API.graphql(graphqlOperation(deleteOrder, { input: { id: orderId } }))
-        )
-      );
-    } catch (error) {
-      console.log("Error deleting orders:", error);
+        .render(paypal.current);
+    };
+
+    document.head.appendChild(script);
+
+    async function deleteOrdersForUser() {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        const userId = user.attributes.sub;
+        const result = await API.graphql(
+          graphqlOperation(listOrders, {
+            filter: {
+              userId: { eq: userId },
+            },
+          })
+        );
+        const orderIds = result.data.listOrders.items.map((item) => item.id);
+        await Promise.all(
+          orderIds.map((orderId) =>
+            API.graphql(
+              graphqlOperation(deleteOrder, { input: { id: orderId } })
+            )
+          )
+        );
+      } catch (error) {
+        console.log("Error deleting orders:", error);
+      }
     }
-  }
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, [
+    value,
+    description,
+    setAlbumsInCart,
+    setCartCount,
+    setPurchased,
+    paypalClientId,
+  ]);
 
-  const paypal = useRef();
-  useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: description,
-                amount: {
-                  currency_code: "USD",
-                  value: value,
-                },
-              },
-            ],
-          });
-        },
-        onApprove: async (data, actions) => {
-          try {
-            await actions.order.capture();
-            setCartCount(0);
-            setAlbumsInCart([]);
-            setPurchased(true);
-            deleteOrdersForUser();
-          } catch (err) {
-            console.log(err);
-          }
-        },
-        onError: (err) => {
-          console.log(err);
-        },
-      })
-      .render(paypal.current);
-  }, [value, description, setAlbumsInCart, setCartCount, setPurchased]);
-
-  return (
-    <div>
-      <div ref={paypal}></div>
-    </div>
-  );
+  return <div ref={paypal}></div>;
 }
