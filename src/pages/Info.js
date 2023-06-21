bedebc653ad7b4a98eb5a78de7d51c6c6fd9ad54
BuildTools <unconfigured@null.spigotmc.org>
2023-06-20 22:53:27 -0500

    better info

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -3,48 +3,50 @@ import "./Info.css";
 export default function Info() {
   return (
     <>
-      <div class="about-section">
+      <div className="about-section">
         <h1>Info Page</h1>
-        <p>Find out different customizable acrylics options!</p>
+        <h5>Find out different customizable acrylic options!</h5>
       </div>
-      <div class="row">
-        <div class="column">
-          <div class="about-cards">
-            <img
-              src="/w3images/team1.jpg"
-              alt="Wall Mount"
-              style={{ width: "100%" }}
-            />
-            <div class="container">
-              <h2>Wall Mount</h2>
-              <p class="title">CEO & Founder</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>jane@example.com</p>
-              <p>
-                <button class="button">Contact</button>
-              </p>
-            </div>
-          </div>
+      <div className="about-cards">
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b2737005885df706891a3c182a57"
+          alt="Wall Mount"
+          style={{ width: "35%" }}
+        />
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b2737005885df706891a3c182a57"
+          alt="Wall Mount"
+          style={{ width: "35%" }}
+        />
+        <div className="container">
+          <h2>Wall Mount*</h2>
+          <p className="title">Example: IGOR by Tyler, The Creator</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         </div>
+      </div>
 
-        <div class="column">
-          <div class="about-cards">
-            <img
-              src="/w3images/team2.jpg"
-              alt="LED Stand"
-              style={{ width: "100%" }}
-            />
-            <div class="container">
-              <h2>LED Stand</h2>
-              <p class="title">Art Director</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>mike@example.com</p>
-              <p>
-                <button class="button">Contact</button>
-              </p>
-            </div>
-          </div>
+      <div className="about-cards">
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163"
+          alt="LED Stand"
+          style={{ width: "35%" }}
+        />
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163"
+          alt="LED Stand"
+          style={{ width: "35%" }}
+        />
+        <div className="container">
+          <h2>LED Stand*</h2>
+          <p className="title">Example: AM by Arctic Monkeys</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         </div>
+      </div>
+      <div
+        className="asterisk"
+        style={{ textAlign: "center", paddingBottom: "20px" }}
+      >
+        <p>*Black & White album covers typically come out better!</p>
       </div>
     </>
   );
